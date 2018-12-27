b9dc47efee35ae2c527b896bb2773d786c69a30f
Luis Chavez <luischavez@richs-mbp.attlocal.net>
2018-12-27 11:06:37 -0800

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -20,23 +20,28 @@ import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
+//
+//// Store SUBSCRIBE to view TEST DATA
+//
 // store.subscribe() does something each time the state changes. In our case console.log.
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
-});
+// store.subscribe(() => {
+//   const state = store.getState();
+//   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//   console.log(visibleExpenses);
+// });
 
-//TEST DATA
-store.dispatch(
-  addExpense({ description: "water bill", amount: 4500, createdAt: 100 })
-);
-store.dispatch(
-  addExpense({ description: "gas bill", amount: 50, createdAt: 1000 })
-);
-store.dispatch(
-  addExpense({ description: "rent", amount: 109500, createdAt: 500 })
-);
+//
+////  TEST DATA
+//
+// store.dispatch(
+//   addExpense({ description: "water bill", amount: 4500, createdAt: 100 })
+// );
+// store.dispatch(
+//   addExpense({ description: "gas bill", amount: 50, createdAt: 1000 })
+// );
+// store.dispatch(
+//   addExpense({ description: "rent", amount: 109500, createdAt: 500 })
+// );
 
 store.dispatch(sortByDate());
 
